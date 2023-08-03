3f5a8d03c9a6d6e2d36419ac2edfa59cb72c56c8
MmmarRTHa <m27842n@outlook.com>
2023-08-02 18:40:17 -0700

    - update: main - solved recursion exercises.

--- bird-count/lib/bird_count.ex
+++ bird-count/lib/bird_count.ex
@@ -1,8 +1,7 @@
 defmodule BirdCount do
-  def today(list) do
-    list
-    |> Enum.at(0)
-  end
+
+    def today([]), do: nil
+    def today([list | _tail]), do: list
 
   def increment_day_count([list | tail]), do: [list + 1 | tail]
   def increment_day_count(_list), do: [1]
@@ -11,11 +10,14 @@ defmodule BirdCount do
   def has_day_without_birds?([_list | tail]), do: has_day_without_birds?(tail)
   def has_day_without_birds?(_list), do: false
 
-  def total(list) do
-    # Please implement the total/1 functionm,./
+  def total([list | tail]) do
+    list + total(tail)
   end
 
-  def busy_days(list) do
-    # Please implement the busy_days/1 function
-  end
+  def total(_list), do: 0
+
+  def busy_days([list | tail]) when list >= 5, do: 1 + busy_days(tail)
+  def busy_days([_list | tail]), do: busy_days(tail)
+  def busy_days(_list), do: 0
+
 end
